c3d82b3fa313096592bfedf47a1c13629c5307f5
Ronny Alvarado <rsalvarado777@gmail.com>
2019-03-30 20:09:02 -0500

    basic styling and form for the front-end; will continue styling later towards the end

--- src/App.js
+++ src/App.js
@@ -1,13 +1,67 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
 
-import './App.css';
+const StyledApp = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background: linear-gradient(to bottom right, lightblue, white);
+  form {
+    border: 1px solid red;
+    margin: 0 auto;
+    display: flex;
+    flex-direction: column;
+    width: 200px;
+  }
+`;
+
+const StyledForm = styled.div`
+  border: 1px solid lightgray;
+  background: white;
+  margin: 0 auto;
+  height: 500px;
+  width: 500px;
+`;
 
 class App extends Component {
+  state = {
+    titleInput: '',
+    descriptionInput: '',
+    results: [], //placeholder for later
+    curPage: []  //placeholder for later
+  }
+
+  changeHandler = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
+
+  submitHandler = () => {
+    //not built yet as I need to figure out how to have this be sent to mongo cloud db
+  }
+
   render() {
     return (
-      <div className="App">
-        
-      </div>
+      <StyledApp>
+        <StyledForm>
+          <form onSubmit={this.submitHandler}>
+            <input 
+            type="text"
+            placeholder="Enter a title..."
+            onChange={this.changeHandler}
+            value={this.state.titleInput} 
+            name="title"
+              />
+            <input 
+              type="text" 
+              placeholder="Enter a description..." 
+              onChange={this.changeHandler} 
+              value={this.state.descriptionInput} 
+              name="description"
+            />
+          </form>
+        </StyledForm>
+      </StyledApp>
     );
   }
 }
